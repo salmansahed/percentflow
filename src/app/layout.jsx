1e464c7f0296b9b832389e72901906bdb96c45f7
salmansahed <salmansahed10@gmail.com>
2026-08-03 09:20:01 +0600

    refactor: enhance layout by adding service worker registration script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${josefinSans.className} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        {/* ⚡ Service Worker Registration Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('/sw.js');
+                });
+              }
+            `,
+          }}
+        />
+      </body>
     </html>
   );
 }
