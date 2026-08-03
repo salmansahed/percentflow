1dcd255f67c6f5dd65c8f76594b49f3587825af0
salmansahed <salmansahed10@gmail.com>
2026-08-03 08:14:19 +0600

    refactor: update layout and metadata for improved clarity and consistency

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,32 +1,20 @@
 import { Geist, Geist_Mono, Josefin_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
 const josefinSans = Josefin_Sans({
   variable: "--font-josefin-sans",
   subsets: ["latin"],
 });
 
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
-  title: "PercentFlow",
+  title: "CalcFlow - Multi-Utility Calculator",
+  description:
+    "A professional percentage, standard, and date calculator suite built with Next.js",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${josefinSans.className} h-full antialiased`}
-    >
+    <html lang="en" className={`${josefinSans.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,5 @@
 import CalcFlowContainer from "@/components/CalcFlowContainer";
 
-export const metadata = {
-  title: "CalcFlow - Multi-Utility Calculator",
-  description:
-    "A professional percentage, standard, and date calculator suite built with Next.js",
-};
-
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-slate-100 p-4 text-slate-800 selection:bg-indigo-500 selection:text-white">
